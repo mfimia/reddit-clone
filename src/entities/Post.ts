48bb1a612219c6faf453d5885334bb8bcc0e50c9
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-01-15 23:28:00 +0100

    exposed fields in graphql

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,17 +1,25 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, ObjectType } from "type-graphql";
 
+// Stacking decorators
+@ObjectType()
 @Entity()
 export class Post {
   // These correspond to columns
+  // Adding the Field decorator will expose them into our graphQL
+  @Field()
   @PrimaryKey()
   id!: number;
 
+  @Field(() => String)
   @Property({ type: "date" })
   createdAt: Date = new Date();
 
+  @Field(() => String)
   @Property({ type: "date", onUpdate: () => new Date() })
   updatedAt: Date = new Date();
 
+  @Field()
   @Property({ type: "text" })
   title!: string;
 }
