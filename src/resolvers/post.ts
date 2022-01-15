8586326072d14f4ca4df0f2e22269155537b5294
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-01-15 23:58:00 +0100

    set up get one post by id

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,14 +1,26 @@
 import { Post } from "../entities/Post";
 import { MyContext } from "src/types";
-import { Ctx, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Int, Query, Resolver } from "type-graphql";
 
 // Decorate the class with resolver from graphql
 @Resolver()
 export class PostResolver {
-  // GraphQL query syntax that returns "hello world"
+  // GraphQL query syntax that returns all posts, using Post entity
   @Query(() => [Post])
   // Take em from context and find all posts
   posts(@Ctx() { em }: MyContext): Promise<Post[]> {
     return em.find(Post, {});
+  }
+  // Get a single post
+  // Type: returns post or null
+  @Query(() => Post, { nullable: true })
+  // Take em from context and find all posts
+  post(
+    // Takes id as argument (we can use the name that we want in the first argument)
+    @Arg("id", () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): // Returns a type promise that is type post or null
+  Promise<Post | null> {
+    return em.findOne(Post, { id });
   }
 }
