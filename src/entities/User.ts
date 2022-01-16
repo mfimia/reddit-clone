d98e1e098c44703bd728ac2a46d567a484735444
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-01-16 10:39:20 +0100

    added user entityto DB

---
+++ src/entities/User.ts
@@ -0,0 +1,28 @@
+import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, ObjectType } from "type-graphql";
+
+// Stacking decorators
+@ObjectType()
+@Entity()
+export class User {
+  // These correspond to columns
+  // Adding the Field decorator will expose them into our graphQL
+  @Field()
+  @PrimaryKey()
+  id!: number;
+
+  @Field(() => String)
+  @Property({ type: "date" })
+  createdAt: Date = new Date();
+
+  @Field(() => String)
+  @Property({ type: "date", onUpdate: () => new Date() })
+  updatedAt: Date = new Date();
+
+  @Field()
+  @Property({ type: "text", unique: true })
+  username!: string;
+
+  @Property({ type: "text" })
+  password!: string;
+}
