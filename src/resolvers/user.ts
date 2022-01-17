7c0cd74a740c2c0de3ef7cd5e37c9d9db598de2d
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-01-17 18:16:02 +0100

    added logout mutation to user resolver

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -11,6 +11,7 @@ import {
   Query,
 } from "type-graphql";
 import argon2 from "argon2";
+import { COOKIE_NAME } from "../constants";
 // ---> Import for alternative query
 // import { EntityManager } from "@mikro-orm/postgresql";
 
@@ -162,5 +163,20 @@ export class UserResolver {
     req.session.userId = user.id;
 
     return { user };
+  }
+
+  @Mutation(() => Boolean)
+  logout(@Ctx() { req, res }: MyContext) {
+    return new Promise((resolve) =>
+      req.session.destroy((err) => {
+        res.clearCookie(COOKIE_NAME);
+        if (err) {
+          console.log(err);
+          resolve(false);
+          return;
+        }
+        resolve(true);
+      })
+    );
   }
 }
