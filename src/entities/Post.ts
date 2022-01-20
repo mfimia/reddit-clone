133cf50c4b47ee6327557afbae7e07544ed9504a
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-01-20 16:52:51 +0100

    added creator and creator id fields

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -4,9 +4,11 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  ManyToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from "typeorm";
+import { User } from "./User";
 
 // Stacking decorators
 @ObjectType()
@@ -19,6 +21,28 @@ export class Post extends BaseEntity {
   @PrimaryGeneratedColumn()
   id!: number;
 
+  @Field()
+  @Column()
+  title!: string;
+
+  @Field()
+  @Column()
+  text!: string;
+
+  @Field()
+  @Column({ type: "int", default: 0 })
+  points!: number;
+
+  // foreign key will be stored in this field
+  @Field()
+  @Column()
+  creatorId: number;
+
+  // set up many to one relationship
+  // many posts can belong to the same user
+  @ManyToOne(() => User, (user) => user.posts)
+  creator: User;
+
   @Field(() => String)
   @CreateDateColumn()
   createdAt: Date;
@@ -26,8 +50,4 @@ export class Post extends BaseEntity {
   @Field(() => String)
   @UpdateDateColumn()
   updatedAt: Date;
-
-  @Field()
-  @Column()
-  title!: string;
 }
